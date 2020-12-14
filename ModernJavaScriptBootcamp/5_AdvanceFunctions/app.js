0d1a56354f81c036046c56e922966f76ded3d1d7
Vidit Shah <ml02100@yahoo.com>
2020-12-15 01:23:20 +0530

    doubleArr

--- ModernJavaScriptBootcamp/5_AdvanceFunctions/app.js
+++ ModernJavaScriptBootcamp/5_AdvanceFunctions/app.js
@@ -3,7 +3,17 @@ let animals = ['grizzly bear', 'panda bear', 'spectacled bear']
 //   console.log(i,animals[i])
 // }
 // console.log(i)
-for (let i = 0; i < animals.length; i++){
-  console.log(i,animals[i])
+// for (let i = 0; i < animals.length; i++){
+//   console.log(i,animals[i])
+// }
+// console.log(i)
+function doubleArr(arr) {
+  const result =[]
+  for (let num of arr) {
+    let double = num * 2;
+    result.push(double)
+  }
+  return result
 }
-console.log(i)
+
+console.log(doubleArr([1,2,3]))
