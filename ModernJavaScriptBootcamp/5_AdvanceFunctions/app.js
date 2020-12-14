bcb18bb51153b0f640eb0509243f8e0271ae4d1b
Vidit Shah <ml02100@yahoo.com>
2020-12-15 01:20:43 +0530

    i error

---
+++ ModernJavaScriptBootcamp/5_AdvanceFunctions/app.js
@@ -0,0 +1,9 @@
+let animals = ['grizzly bear', 'panda bear', 'spectacled bear']
+// for (var i = 0; i < animals.length; i++){
+//   console.log(i,animals[i])
+// }
+// console.log(i)
+for (let i = 0; i < animals.length; i++){
+  console.log(i,animals[i])
+}
+console.log(i)
---
+++ ModernJavaScriptBootcamp/5_AdvanceFunctions/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+for (var i = 0; i < animals.length; i++){
+//   console.log(i,animals[i])
+// }
+// console.log(i)
