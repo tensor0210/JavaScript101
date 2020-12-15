a435eba2893316a7820abb4c7c05f1d474326cdc
Vidit Shah <ml02100@yahoo.com>
2020-12-15 14:00:12 +0530

    higherOrderFunc1

--- ModernJavaScriptBootcamp/5_AdvanceFunctions/app.js
+++ ModernJavaScriptBootcamp/5_AdvanceFunctions/app.js
@@ -25,20 +25,51 @@
 //   inner()
 // }
 
-const add = function (x, y) {
-  return x + y;
-}
-const subtract = function (x, y) {
-  return x - y;
-}
-const multiply = function (x, y) {
-  return x * y;
-}
-const divide = function (x, y) {
-  return x / y;
-}
-const operations = [add, subtract, multiply, divide]
-for (let func of operations) {
-  let result = func(30, 5);
-  console.log(result);
-}
+// const add = function (x, y) {
+//   return x + y;
+// }
+// const subtract = function (x, y) {
+//   return x - y;
+// }
+// const multiply = function (x, y) {
+//   return x * y;
+// }
+// const divide = function (x, y) {
+//   return x / y;
+// }
+// const operations = [add, subtract, multiply, divide]
+// for (let func of operations) {
+//   let result = func(30, 5);
+//   console.log(result);
+// } 
+
+// function callTwice(func) {
+//   func();
+//   func();
+// }
+// function cry() {
+//   console.log("BOO HOO I'M SO SAD");
+// }
+
+// function rage() {
+//   console.log("I hate everything!")
+// }
+
+// // callTwice(cry)
+// // callTwice(rage)
+
+// function repeaNTimes(action, num) {
+//   for (let i = 0; i < num; i++){
+//     action()
+//   }
+// }
+
+// // repeaNTimes(rage,13)
+// // repeaNTimes(cry, 100)
+
+// function pickOne(f1, f2) {
+//   let rand= Math.random()
+//   if (rand < 0.5) { f1() } else { f2() }
+  
+// }
+// pickOne(cry,rage)
