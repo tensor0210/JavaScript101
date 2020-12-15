13ef5aea5cbfb9446339c70d4138ad0f4cee38c5
Vidit Shah <ml02100@yahoo.com>
2020-12-16 01:44:24 +0530

    primitive

--- CompleteJS/01-Fundamentals-Part-1/starter/script.js
+++ CompleteJS/01-Fundamentals-Part-1/starter/script.js
@@ -5,3 +5,20 @@
 // console.log(firstName);
 // let myFirstJob = 'Coder';
 // let myCurrentJob = 'Teacher';
+/*
+*
+ * Multi Line Comment
+ * 
+ */
+// console.log(typeof true);
+// console.log(typeof 23);
+// console.log(typeof 'Vidit');
+// console.log(typeof 45.6);
+// let javaScriptIsFun = true;
+// javaScriptIsFun = 'Yes';
+// console.log(typeof javaScriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+// console.log(typeof null);
