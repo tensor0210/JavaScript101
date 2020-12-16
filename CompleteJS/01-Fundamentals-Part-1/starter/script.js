e9596689917285d9482847372ffe6014bf657e40
Vidit Shah <ml02100@yahoo.com>
2020-12-16 14:32:53 +0530

    templateLiterals

--- CompleteJS/01-Fundamentals-Part-1/starter/script.js
+++ CompleteJS/01-Fundamentals-Part-1/starter/script.js
@@ -75,3 +75,12 @@
 // const markHigherBMI = BMIMark > BMIJohn;
 
 // console.log(BMIMark, BMIJohn, markHigherBMI);
+
+//Strings
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+const jonas = `I'm ${firstName} a ${year - birthYear} years old  ${job}`;
+console.log(jonas);
