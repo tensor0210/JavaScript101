9886217cc00899bed412380e1648738bdd657878
Vidit Shah <ml02100@yahoo.com>
2020-12-16 14:21:55 +0530

    precedance

--- CompleteJS/01-Fundamentals-Part-1/starter/script.js
+++ CompleteJS/01-Fundamentals-Part-1/starter/script.js
@@ -27,33 +27,39 @@
 // const birthYear = 1991;
 //Cannot Declare empty const variables
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2020;
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2020;
 
-console.log(ageJonas, ageSarah);
-console.log(ageJonas * 2);
-console.log(ageJonas / 10);
-console.log(ageJonas ** 2);
+// console.log(ageJonas, ageSarah);
+// console.log(ageJonas * 2);
+// console.log(ageJonas / 10);
+// console.log(ageJonas ** 2);
 
-const firstName = 'Vidit';
-const lastName = 'Shah';
+// const firstName = 'Vidit';
+// const lastName = 'Shah';
 
-console.log(firstName + ' ' + lastName);
+// console.log(firstName + ' ' + lastName);
 
-let x = 10 + 5;
-console.log(x);
-x += 10;
-console.log(x);
-x *= 4;
-console.log(x);
-x++;
-console.log(x);
-x--;
-x--;
-console.log(x);
-//Comparison Operator
-console.log(ageJonas > ageSarah);
-console.log(ageSarah >= 18);
-const isFullAge = ageSarah >= 18;
-console.log(now - 1991 > now - 2018);
+// let x = 10 + 5;
+// console.log(x);
+// x += 10;
+// console.log(x);
+// x *= 4;
+// console.log(x);
+// x++;
+// console.log(x);
+// x--;
+// x--;
+// console.log(x);
+// //Comparison Operator
+// console.log(ageJonas > ageSarah);
+// console.log(ageSarah >= 18);
+// const isFullAge = ageSarah >= 18;
+// console.log(now - 1991 > now - 2018);
+// let x, y;
+// x = y = 25 - 10 - 5;
+// console.log(x, y);
+
+// const avgAge = (ageJonas + ageSarah) / 2;
+// console.log(avgAge);
