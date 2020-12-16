6dc3ac4769be9498727131f249c90af18838bf6f
Vidit Shah <ml02100@yahoo.com>
2020-12-16 14:13:19 +0530

    variables

--- CompleteJS/01-Fundamentals-Part-1/starter/script.js
+++ CompleteJS/01-Fundamentals-Part-1/starter/script.js
@@ -22,7 +22,38 @@
 // console.log(year);
 // console.log(typeof year);
 // console.log(typeof null);
-let age = 30;
-age = 31;
-const birthYear = 1991;
+// let age = 30;
+// age = 31;
+// const birthYear = 1991;
 //Cannot Declare empty const variables
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2);
+console.log(ageJonas / 10);
+console.log(ageJonas ** 2);
+
+const firstName = 'Vidit';
+const lastName = 'Shah';
+
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+console.log(x);
+x += 10;
+console.log(x);
+x *= 4;
+console.log(x);
+x++;
+console.log(x);
+x--;
+x--;
+console.log(x);
+//Comparison Operator
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
